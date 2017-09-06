4f21de3ca5b16d2e2cf8239df023bf975c122612
Stacy Lau <31710677+stacy-lau@users.noreply.github.com>
2017-09-06 16:24:43 -0700

    Update media.py

--- media.py
+++ media.py
@@ -3,8 +3,6 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie-related information"""
     
-    VALID_RATING = ["G", "PG", "PG-13", "R"]
-    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
